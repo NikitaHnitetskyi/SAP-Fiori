24a330b78fe8a60ee28dd2d724de0088fd75e4e3
Nikita Hnitetskyi <nhnitetskyi@eva.ua>
2024-05-13 14:14:51 +0000

    step 30

--- webapp/controller/InvoiceList.controller.js
+++ webapp/controller/InvoiceList.controller.js
@@ -1,34 +1,39 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller",
-	"sap/ui/model/json/JSONModel",
-	"../model/formatter",
-	"sap/ui/model/Filter",
-	"sap/ui/model/FilterOperator"
-], (Controller, JSONModel, formatter, Filter, FilterOperator) => {
-	"use strict";
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel",
+    "../model/formatter",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator"
+], function(Controller, JSONModel, formatter, Filter, FilterOperator) {
+    "use strict";
 
-	return Controller.extend("ui5.walkthrough.controller.InvoiceList", {
-		formatter: formatter, 
+    return Controller.extend("ui5.walkthrough.controller.InvoiceList", {
+        formatter: formatter,
 
-		onInit() {
-			const oViewModel = new JSONModel({
-				currency: "EUR"
-			});
-			this.getView().setModel(oViewModel, "view");
-		},
+        onInit: function() {
+            var oViewModel = new JSONModel({
+                currency: "EUR"
+            });
+            this.getView().setModel(oViewModel, "view");
+        },
 
-		onFilterInvoices(oEvent) {
-			// build filter array
-			const aFilter = [];
-			const sQuery = oEvent.getParameter("query");
-			if (sQuery) {
-				aFilter.push(new Filter("ProductName", FilterOperator.Contains, sQuery));
-			}
+        onFilterInvoices: function(oEvent) {
+            // Build filter array
+            var aFilter = [];
+            var sQuery = oEvent.getParameter("query");
+            if (sQuery) {
+                aFilter.push(new Filter("ProductName", FilterOperator.Contains, sQuery));
+            }
 
-			// filter binding
-			const oList = this.byId("invoiceList");
-			const oBinding = oList.getBinding("items");
-			oBinding.filter(aFilter);
-		}
-	});
+            // Filter binding
+            var oList = this.byId("invoiceList");
+            var oBinding = oList.getBinding("items");
+            oBinding.filter(aFilter);
+        },
+
+        onPress: function() {
+            var oRouter = this.getOwnerComponent().getRouter();
+            oRouter.navTo("detail");
+        }
+    });
 });
